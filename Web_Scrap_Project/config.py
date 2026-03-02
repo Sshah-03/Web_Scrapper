2e64b60eb215f462a6ec21b008c6d4a82f54e148
Sshah-03 <sshah@griddynamics.com>
2026-03-02 15:53:14 +0530

    Made changes

--- Web_Scrap_Project/config.py
+++ Web_Scrap_Project/config.py
@@ -1,9 +1,9 @@
-CACHE_TYPE = "file"   # "redis" or "file"
+CACHE_TYPE = "file"
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 
 REQUEST_TIMEOUT = 10
-RATE_LIMIT = 5   # requests per second
-CACHE_TTL = 300  # seconds
+RATE_LIMIT = 5
+CACHE_TTL = 300 
 
 OUTPUT_FILE = "scraped_data.csv"
